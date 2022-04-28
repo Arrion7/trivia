8e5c301083ca711f10e1b66afe88214567218fc7
Austin Burns <austin012@revature.net>
2022-04-28 12:24:56 -0400

    Working on the page/button layouts

--- FrontEnd/AngularTrivia/src/app/app-routing.module.ts
+++ FrontEnd/AngularTrivia/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { GamesComponent } from './games/games.component';
+
 
 const routes: Routes = [
   {path:"", redirectTo:"welcome",pathMatch:"full"},
+  {path:"homepage", component:HomepageComponent},
   {path:"welcome", component:WelcomeComponent},
-  {path:"questions", component:QuestionsComponent}
+  {path:"questions", component:QuestionsComponent},
+  {path:"login", component:LoginComponent},
+  {path:"games", component:GamesComponent}
 ];
 
 @NgModule({
