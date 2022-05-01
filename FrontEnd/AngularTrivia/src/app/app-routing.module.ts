89f201dea4fa79e74acb5f7bb36ffe32fe374f4b
NovaFlash321 <novaflash321@gmail.com>
2022-05-01 16:30:40 -0400

    more stuff

--- FrontEnd/AngularTrivia/src/app/app-routing.module.ts
+++ FrontEnd/AngularTrivia/src/app/app-routing.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { LoginComponent } from './connexion/login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { LoginComponent } from './login/login.component';
-import { GamesComponent } from './games/games.component';
-import { CategoryComponent } from './category/category.component';
-
-
+import { RegisterComponent } from './connexion/register/register.component';
 
 const routes: Routes = [
-  {path:"", redirectTo:"welcome",pathMatch:"full"},
-  {path:"homepage", component:HomepageComponent},
-  {path:"welcome", component:WelcomeComponent},
-  {path:"questions", component:QuestionsComponent},
+  {path:"", redirectTo:"login",pathMatch:"full"},
   {path:"login", component:LoginComponent},
-  {path:"games", component:GamesComponent},
-  {path:"category", component:CategoryComponent}
-
-
+  {path:"register", component:RegisterComponent},
+  {path:"welcome", component:WelcomeComponent},
+  {path:"questions", component:QuestionsComponent}
 ];
 
 @NgModule({
